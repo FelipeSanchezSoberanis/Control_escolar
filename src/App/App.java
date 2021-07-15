eaa03d59c9644bc8292efa1391b511492c193f54
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2021-07-15 13:49:02 -0500

    Funcionalidad de tabs en el menú
    Se agregó un menú funciona en la parte de arriba, así como cada una de
    sus respectivas tabs y una tab especial para cuando se inicializa el
    programa.

--- src/App/App.java
+++ src/App/App.java
@@ -1,31 +1,8 @@
 package App;
 
-import java.util.List;
-
 public class App {
     public static void main(String[] args) {
-        ControlEscolar.getData();
-        List<String[]> promediosTotales = ControlEscolar.getPromedioTotalAlumnos();
-        List<String[]> promediosParciales = ControlEscolar.getPromedioParcialAlumnos();
-        List<String[]> alumnosConReprobadas = ControlEscolar.getAlumnosYReprobadas();
-        List<String[]> extraordinarios = ControlEscolar.getExtraordinarios();
-
-
-        for (String[] promedio : promediosTotales) {
-            System.out.println("Matricula: " + promedio[0] + " | Promedio Total: " + promedio[1]);
-        }
-
-        for (String[] promedio : promediosParciales) {
-            System.out.println("Matricula: " + promedio[0] + " | Promedio Parcial: " + promedio[1]);
-        }
-
-        for (String[] alumno : alumnosConReprobadas) {
-            System.out.println("Matricula: " + alumno[0] + " | Clave: " + alumno[1]);
-        }
-
-        for (String[] extraordinario : extraordinarios) {
-            System.out.println("Clave: " + extraordinario[0] + " | Extraordinarios: " + extraordinario[1]);
-        }
-
+        appWindow window = new appWindow();
+        window.setVisible(true);
     }
 }
